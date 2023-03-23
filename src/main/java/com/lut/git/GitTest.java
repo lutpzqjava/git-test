8313a18c638dbff86d3591fb080b38962370aead
PengZhiqiang <2734796024@qq.com>
2023-03-23 16:24:25 +0800

    second commit

--- src/main/java/com/lut/git/GitTest.java
+++ src/main/java/com/lut/git/GitTest.java
@@ -3,5 +3,6 @@ package com.lut.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("second commit");
     }
 }
